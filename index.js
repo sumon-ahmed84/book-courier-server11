368e48a502dfd4da320c7a4b3861c3e34664cda1
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-12-15 14:42:34 +0600

    all order save in mongodb api create

--- index.js
+++ index.js
@@ -1,102 +1,81 @@
-require("dotenv").config();
-const express = require("express");
-
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const admin = require("firebase-admin");
-
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Decode Firebase service account key
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf-8");
-const serviceAccount = JSON.parse(decoded);
-
-// Initialize Firebase admin
+const admin = require('firebase-admin')
+const port = process.env.PORT || 5000
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
+  'utf-8'
+)
+const serviceAccount = JSON.parse(decoded)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
+})
 
-// Middleware
+const app = express()
+// middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173",
-      "http://localhost:5174",
-      process.env.CLIENT_DOMAIN], 
+    origin: [process.env.CLIENT_DOMAIN],
     credentials: true,
     optionSuccessStatus: 200,
   })
-);
-app.use(express.json());
+)
+app.use(express.json())
 
-// JWT middleware
+// jwt middlewares
 const verifyJWT = async (req, res, next) => {
-  const token = req?.headers?.authorization?.split(" ")[1];
-  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+  const token = req?.headers?.authorization?.split(' ')[1]
+  console.log(token)
+  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
   try {
-    const decoded = await admin.auth().verifyIdToken(token);
-    req.tokenEmail = decoded.email;
-    next();
+    const decoded = await admin.auth().verifyIdToken(token)
+    req.tokenEmail = decoded.email
+    console.log(decoded)
+    next()
   } catch (err) {
-    return res.status(401).send({ message: "Unauthorized Access!", err });
+    console.log(err)
+    return res.status(401).send({ message: 'Unauthorized Access!', err })
   }
-};
+}
 
-// Connect to MongoDB
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
-});
-
+})
 async function run() {
   try {
     const db = client.db("booksDB");
     const booksCollection = db.collection("books");
+    const ordersCollection = db.collection('orders')
 
-    // Ping MongoDB
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected to MongoDB successfully!");
+    // Save a book data in db
+    app.post('/books', async (req, res) => {
+      const bookData = req.body
+      console.log(bookData)
+      const result = await booksCollection.insertOne(bookData)
+      res.send(result)
+    })
 
-    // POST /books -> add a book
-    app.post("/books", async (req, res) => {
-      const bookData = req.body;
-      const result = await booksCollection.insertOne(bookData);
-      res.send(result);
-    });
+    // get all books from db
+    app.get('/books', async (req, res) => {
+      const result = await booksCollection.find().toArray()
+      res.send(result)
+    })
 
-    // GET /books -> get all books
-    app.get("/books", async (req, res) => {
-      const result = await booksCollection.find().toArray();
-      res.send(result);
-    });
+    // get all books from db
+    app.get('/books/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await booksCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
 
-    // GET /books/:id -> get book by ID
-    app.get("/books/:id", async (req, res) => {
-      const id = req.params.id;
-
-      if (!ObjectId.isValid(id)) {
-        return res.status(400).send({ error: "Invalid ID" });
-      }
-
-      const result = await booksCollection.findOne({ _id: new ObjectId(id) });
-      if (!result) {
-        return res.status(404).send({ error: "Book not found" });
-      }
-
-      res.send(result);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-
- // Payment endpoints
+    // Payment endpoints
     app.post('/create-checkout-session', async (req, res) => {
       const paymentInfo = req.body
       console.log(paymentInfo)
@@ -118,23 +97,234 @@ async function run() {
         customer_email: paymentInfo?.customer?.email,
         mode: 'payment',
         metadata: {
-          plantId: paymentInfo?.plantId,
+          bookId: paymentInfo?.bookId,
           customer: paymentInfo?.customer.email,
         },
         success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.CLIENT_DOMAIN}/plant/${paymentInfo?.plantId}`,
+        cancel_url: `${process.env.CLIENT_DOMAIN}/book/${paymentInfo?.bookId}`,
       })
       res.send({ url: session.url })
     })
 
-run().catch(console.dir);
+    app.post('/payment-success', async (req, res) => {
+      const { sessionId } = req.body
+      const session = await stripe.checkout.sessions.retrieve(sessionId)
+      const book = await booksCollection.findOne({
+        _id: new ObjectId(session.metadata.bookId),
+      })
+      const order = await ordersCollection.findOne({
+        transactionId: session.payment_intent,
+      })
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Hello from Server..");
-});
+      if (session.status === 'complete' && book && !order) {
+        // save order data in db
+        const orderInfo = {
+          bookId: session.metadata.bookId,
+          transactionId: session.payment_intent,
+          customer: session.metadata.customer,
+          status: 'pending',
+          seller: book.seller,
+          name: book.name,
+          category: book.category,
+          quantity: 1,
+          price: session.amount_total / 100,
+          image: book?.image,
+        }
+        const result = await ordersCollection.insertOne(orderInfo)
+        // update book quantity
+        await booksCollection.updateOne(
+          {
+            _id: new ObjectId(session.metadata.bookId),
+          },
+          { $inc: { quantity: -1 } }
+        )
 
-// Start server
+        return res.send({
+          transactionId: session.payment_intent,
+          orderId: result.insertedId,
+        })
+      }
+      res.send(
+        res.send({
+          transactionId: session.payment_intent,
+          orderId: order._id,
+        })
+      )
+    })
+
+    // get all orders for a customer by email
+    app.get('/my-orders/:email', async (req, res) => {
+      const email = req.params.email
+
+      const result = await ordersCollection.find({ customer: email }).toArray()
+      res.send(result)
+    })
+
+    
+
+
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from Server..')
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
+
+
+
+
+// require("dotenv").config();
+// const express = require("express");
+
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+// const cors = require("cors");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const admin = require("firebase-admin");
+
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // Decode Firebase service account key
+// const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf-8");
+// const serviceAccount = JSON.parse(decoded);
+
+// // Initialize Firebase admin
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// // Middleware
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173",
+//       "http://localhost:5174",
+//       process.env.CLIENT_DOMAIN], 
+//     credentials: true,
+//     optionSuccessStatus: 200,
+//   })
+// );
+// app.use(express.json());
+
+// // JWT middleware
+// const verifyJWT = async (req, res, next) => {
+//   const token = req?.headers?.authorization?.split(" ")[1];
+//   if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+//   try {
+//     const decoded = await admin.auth().verifyIdToken(token);
+//     req.tokenEmail = decoded.email;
+//     next();
+//   } catch (err) {
+//     return res.status(401).send({ message: "Unauthorized Access!", err });
+//   }
+// };
+
+// // Connect to MongoDB
+// const client = new MongoClient(process.env.MONGODB_URI, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     const db = client.db("booksDB");
+//     const booksCollection = db.collection("books");
+
+//     // Ping MongoDB
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected to MongoDB successfully!");
+
+//     // POST /books -> add a book
+//     app.post("/books", async (req, res) => {
+//       const bookData = req.body;
+//       const result = await booksCollection.insertOne(bookData);
+//       res.send(result);
+//     });
+
+//     // GET /books -> get all books
+//     app.get("/books", async (req, res) => {
+//       const result = await booksCollection.find().toArray();
+//       res.send(result);
+//     });
+
+//     // GET /books/:id -> get book by ID
+//     app.get("/books/:id", async (req, res) => {
+//       const id = req.params.id;
+
+//       if (!ObjectId.isValid(id)) {
+//         return res.status(400).send({ error: "Invalid ID" });
+//       }
+
+//       const result = await booksCollection.findOne({ _id: new ObjectId(id) });
+//       if (!result) {
+//         return res.status(404).send({ error: "Book not found" });
+//       }
+
+//       res.send(result);
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+
+//  // Payment endpoints
+//     app.post('/create-checkout-session', async (req, res) => {
+//       const paymentInfo = req.body
+//       console.log(paymentInfo)
+//       const session = await stripe.checkout.sessions.create({
+//         line_items: [
+//           {
+//             price_data: {
+//               currency: 'usd',
+//               product_data: {
+//                 name: paymentInfo?.name,
+//                 description: paymentInfo?.description,
+//                 images: [paymentInfo.image],
+//               },
+//               unit_amount: paymentInfo?.price * 100,
+//             },
+//             quantity: paymentInfo?.quantity,
+//           },
+//         ],
+//         customer_email: paymentInfo?.customer?.email,
+//         mode: 'payment',
+//         metadata: {
+//           bookId: paymentInfo?.bookId,
+//           customer: paymentInfo?.customer.email,
+//         },
+//         success_url: `http://localhost:5173/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+//         cancel_url: `http://localhost:5173/book/${paymentInfo?.bookId}`,
+//       })
+//       res.send({ url: session.url })
+//     })
+
+// run().catch(console.dir);
+
+// // Test route
+// app.get("/", (req, res) => {
+//   res.send("Hello from Server..");
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
